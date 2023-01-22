51c7a1f7700bc7443b58120df7caf604f8683367
ttoppl <78095158+ttoppl@users.noreply.github.com>
2023-01-22 08:17:43 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+-- made by ttoppl
+
+_G.AimbotEnabled = true
+_G.TeamCheck = true
+_G.AimPart = "Head"
+_G.Sensitivity = 0
+
+_G.CircleSides = 64
+_G.CircleColor = Color3.fromRGB(255,255,255)
+_G.CircleTransparency = 0.9
+_G.CircleRadius = 80
+_G.CircleFilled = false
+_G.CircleVisible = true
+_G.CircleThickness = 0
+
+loadstring(game:HttpGet("https://pastebin.com/raw/t3KaYGNe", true))()
